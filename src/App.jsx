20a0b892c5e1b88ccde7068e4649b538419561aa
Suhani2205 <155963315+Suhani2205@users.noreply.github.com>
2025-07-06 17:11:52 +0530

    edited images in readMe-2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
 import Hero from './components/custom/Hero'
+import { useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
