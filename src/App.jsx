346757e08cbc8b0a7ecfd4f7ffb4af2a8d7a28c4
Suhani2205 <155963315+Suhani2205@users.noreply.github.com>
2025-07-06 17:16:56 +0530

    undo changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
 import Hero from './components/custom/Hero'
-import { useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
-  useEffect(() => {
-  console.log('Maps key at runtime:', import.meta.env.VITE_GOOGLE_PLACE_API_KEY);
-}, []);
+
 
 
   return (
