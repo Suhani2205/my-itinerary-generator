ea192a490f1e2006c4b8cad9f39485f67da07035
Suhani2205 <155963315+Suhani2205@users.noreply.github.com>
2025-07-07 13:13:09 +0530

    Edited header (Added user image and logout feature)

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Button } from '../ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { googleLogout } from '@react-oauth/google';
+// import { useNavigate, useNavigation } from 'react-router-dom';
 
 function Header() {
-  // const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user'));
+  // const navigation = useNavigation();
 
-  // useEffect(()=>{
-  //   console.log(user);
-  // },[])
+  useEffect(()=>{
+    console.log(user);
+  },[])
   return (
     <div className='p-3 shadow-sm flex justify-between items-center px-5'>
         <img src="/logo.svg" alt="" />
         <div>
-            <Button>Sign Up</Button>
+            {user ?
+              <div className='flex items-centre gap-3'> 
+                <Button variant="outline" className = "rounded-full"> My Trips</Button>
+
+                
+                <Popover>
+                  <PopoverTrigger><img src = {user.picture} className='h-[25px] w-[25px] rounded-full'/></PopoverTrigger>
+                  <PopoverContent>
+                    <h2 onClick={()=>{
+                        googleLogout();
+                        localStorage.clear();
+                      // navigation('\');
+                      window.location.reload();
+                    
+                    }}>
+                      Logout
+                    </h2>
+                  </PopoverContent>
+                </Popover>
+              </div>
+              :
+              <Button>Sign Up</Button>
+            }
+
         </div>
     </div>
   )
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -119,6 +119,7 @@ const SaveAiTrip = async (TripData) => {
 };
 
 
+
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
